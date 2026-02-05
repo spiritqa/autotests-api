49703b8dead9d113e8f6264b71d96cf20bdeb869
spiritqa <spirit.bear@hotmail.com>
2026-02-05 23:08:43 +0300

    Знакомство с gRPC протоколом

--- grpc_server.py
+++ grpc_server.py
@@ -16,6 +16,7 @@ def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     user_service_pb2_grpc.add_UserServiceServicer_to_server(UserServiceServicer(), server)
     server.add_insecure_port('[::]:50051')
+    server.start()
     print("gRPC сервер запущен на порту 50051...")
     server.wait_for_termination()
 
