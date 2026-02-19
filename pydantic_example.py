4aab61f345444b77a4f3b1c8ca481b5de825adc9
spiritqa <spirit.bear@hotmail.com>
2026-02-19 21:13:28 +0300

    Знакомство с Pydantic

---
+++ pydantic_example.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, Field
+
+
+class Address(BaseModel):
+    city: str
+    zip_code: str
+
+class User(BaseModel):
+    id: int
+    name: str
+    email: str
+    is_active: bool = Field(alias="isActive")
+
+user_data ={
+    'id':1,
+    'name':'Alice',
+    'email':'alice@example.com',
+    'isActive': True
+}
+user = User(**user_data)
+print(user.model_dump())
