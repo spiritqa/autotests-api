25635104bde3249402cefe8de40db25527fd9a36
spiritqa <spirit.bear@hotmail.com>
2026-02-24 20:47:00 +0300

    Знакомство с Faker. Практика работы с генерацией случайных данных

--- api_client_create_exercise.py
+++ api_client_create_exercise.py
@@ -7,17 +7,10 @@ from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import CreateUserRequestSchema
 from clients.files.files_client import get_files_client
 from clients.files.files_schema import CreateFileRequestSchema
-from tools.fakers import fake
 
 public_users_client = get_public_users_client()
 
-create_user_request = CreateUserRequestSchema(
-    email=fake.email(),
-    password="stringPass",
-    last_name="stringLast",
-    first_name="stringFirst",
-    middle_name="stringMiddle"
-)
+create_user_request = CreateUserRequestSchema()
 create_user_response = public_users_client.create_user(create_user_request)
 
 authentication_user = AuthenticationUserSchema(
@@ -26,22 +19,12 @@ authentication_user = AuthenticationUserSchema(
 )
 
 files_client = get_files_client(authentication_user)
-create_file_request = CreateFileRequestSchema(
-    filename='image.png',
-    directory='courses',
-    upload_file='./testdata/files/image.png'
-
-)
+create_file_request = CreateFileRequestSchema(upload_file='./testdata/files/image.png')
 create_file_response = files_client.create_file(create_file_request)
 print('Create file data: ', create_file_request)
 
 course_client = get_courses_client(authentication_user)
 create_courses_request = CreateCourseRequestSchema(
-    title="Python3.13",
-    max_score=200,
-    min_score=30,
-    description="Python course",
-    estimated_time="23 year",
     preview_file_id=create_file_response.file.id,
     created_by_user_id=create_user_response.user.id
 
@@ -51,13 +34,7 @@ print('Create course data', create_courses_response)
 
 exercises_client = get_exercises_client(authentication_user)
 create_exercises_request = CreateExercisesRequestSchema(
-    title="Python start",
-    course_id=create_courses_response.course.id,
-    max_score=3,
-    min_score=2,
-    order_index=1,
-    description="3,2,1...GO",
-    estimated_time="1s"
+    course_id=create_courses_response.course.id
 )
 create_exercises_response = exercises_client.create_exercise(create_exercises_request)
 print("Create exercise data: ", create_exercises_response)
--- clients/exercises/exercises_schema.py
+++ clients/exercises/exercises_schema.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, Field, ConfigDict
+from tools.fakers import  fake
 
 
 class ExerciseSchema(BaseModel):
@@ -37,13 +38,13 @@ class CreateExercisesRequestSchema(BaseModel):
     """
     model_config = ConfigDict(populate_by_name=True)
 
-    title: str
-    course_id: str = Field(alias="courseId")
-    max_score: int = Field(alias="maxScore")
-    min_score: int = Field(alias="minScore")
-    order_index: int = Field(alias="orderIndex")
-    description: str
-    estimated_time: str = Field(alias="estimatedTime")
+    title: str =Field(default_factory=fake.sentence)
+    course_id: str = Field(alias="courseId", default_factory=fake.uuid4)
+    max_score: int = Field(alias="maxScore", default_factory=fake.max_score)
+    min_score: int = Field(alias="minScore", default_factory=fake.min_score)
+    order_index: int = Field(alias="orderIndex", default_factory=fake.integer)
+    description: str = Field(default_factory=fake.text)
+    estimated_time: str = Field(alias="estimatedTime", default_factory=fake.estimated_time)
 
 class CreateExercisesResponseSchema(BaseModel):
     """
@@ -71,12 +72,12 @@ class UpdateExercisesRequestSchema(BaseModel):
     """
     model_config = ConfigDict(populate_by_name=True)
 
-    title: str | None
-    max_score: int = Field(alias="maxScore")
-    min_score: int = Field(alias="minScore")
-    order_index: int = Field(alias="orderIndex")
-    description: str
-    estimated_time: str = Field(alias="estimatedTime")
+    title: str | None  = Field(default_factory=fake.sentence)
+    max_score: int = Field(alias="maxScore", default_factory=fake.max_score)
+    min_score: int = Field(alias="minScore", default_factory=fake.min_score)
+    order_index: int = Field(alias="orderIndex", default_factory=fake.integer)
+    description: str = Field(default_factory=fake.text)
+    estimated_time: str = Field(alias="estimatedTime", default_factory=fake.estimated_time)
 
 class UpdateExercisesResponseSchema(BaseModel):
     """
