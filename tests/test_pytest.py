16526cadbda474b8560aebd9ffd27a018fdd3aad
spiritqa <spirit.bear@hotmail.com>
2026-02-25 19:51:12 +0300

    Введение в Pytest

---
+++ tests/test_pytest.py
@@ -0,0 +1,14 @@
+def test_first_try():
+    print("Hello World!")
+
+
+
+def test_assert_positive_case():
+    assert 2 + 2 == 4
+
+
+def test_assert_negative_case():
+    x =5
+    y = 2
+    assert y + y == x , "2 + 2 is not equals to 5"
+
